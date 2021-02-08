24b8fdd67356c74da905c4d1e5549abd50b9b6bb
dinar0 <dinar21@mail.ru>
2021-02-08 06:30:53 +0300

    дз 1 исправил названия SomeData и SecondActivity.kt

---
+++ app/src/main/java/ru/otus/otusandroidbasic/FilmDetals.kt
@@ -0,0 +1,33 @@
+package ru.otus.otusandroidbasic
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import ru.otus.otusandroidbasic.MainActivity.Companion.COMMENT
+import ru.otus.otusandroidbasic.MainActivity.Companion.LIKE
+
+class FilmDetals : AppCompatActivity() {
+    companion object {
+        const val EXTRA_Data = "EXTRA_Data"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+        intent.getParcelableExtra<FilmData>(EXTRA_Data)?.let {
+            findViewById<TextView>(R.id.title).setText(it.resTit)
+            findViewById<TextView>(R.id.text).setText(it.resTxt)
+            findViewById<ImageView>(R.id.imageView).setImageResource(it.resImg)
+        }
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener { view: View? ->
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra(LIKE, (findViewById<View>(R.id.likeCheckBox) as CheckBox).isChecked)
+            intent.putExtra(COMMENT, (findViewById<View>(R.id.comment) as EditText).text.toString())
+            setResult(RESULT_OK, intent)
+            finish()
+        }
+    }
+}
--- app/src/main/java/ru/otus/otusandroidbasic/MainActivity.kt
+++ app/src/main/java/ru/otus/otusandroidbasic/MainActivity.kt
@@ -4,15 +4,19 @@ import android.content.Intent
 import android.graphics.Color.BLACK
 import android.graphics.Color.BLUE
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import ru.otus.otusandroidbasic.SecondActivity.Companion.EXTRA_Data
+import ru.otus.otusandroidbasic.FilmDetals.Companion.EXTRA_Data
+
 
 class MainActivity : AppCompatActivity() {
     companion object {
         const val IDFILM = "selected_film"
-
+        const val LIKE= "like"
+        const val COMMENT= "comment"
+       // const val REQUEST_FOR_COMMENT = 1
     }
 
     private val textView1 by lazy { findViewById<TextView>(R.id.textView) }
@@ -23,37 +27,68 @@ class MainActivity : AppCompatActivity() {
     private val button3 by lazy { findViewById<View>(R.id.button3) }
     private val button4 by lazy { findViewById<View>(R.id.button4) }
     var IdBtn: Int = 0
+    var like = false
+    var comment: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         if (savedInstanceState != null) {
-            IdBtn = savedInstanceState.getInt(MainActivity.IDFILM)
+            IdBtn = savedInstanceState.getInt(IDFILM)
         }
         changeTextColors(IdBtn)
         onClickListener()
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        intent.getStringExtra(LIKE).let{ like=true}
+        intent.getStringExtra(COMMENT).let{ comment="dsdsddsdscs"}
+        Log.i("Result", "Liked: $like  comment: $comment")
     }
 
     private fun onClickListener() {
         button1.setOnClickListener {
             IdBtn = 1
             changeTextColors(IdBtn)
-            val intent = Intent(this, SecondActivity::class.java)
-            intent.putExtra(EXTRA_Data, SomeData(IdBtn, R.drawable.g, R.string.G_text))
+            val intent = Intent(this, FilmDetals::class.java)
+            intent.putExtra(
+                EXTRA_Data, FilmData(
+                    IdBtn,
+                    R.drawable.g,
+                    R.string.G_text,
+                    R.string.gentl
+                )
+            )
             startActivity(intent)
         }
         button2.setOnClickListener {
             IdBtn = 2
             changeTextColors(IdBtn)
-            val intent = Intent(this, SecondActivity::class.java)
-            intent.putExtra(EXTRA_Data, SomeData(IdBtn, R.drawable.l, R.string.L_text))
+            val intent = Intent(this, FilmDetals::class.java)
+            intent.putExtra(
+                EXTRA_Data, FilmData(
+                    IdBtn,
+                    R.drawable.l,
+                    R.string.L_text,
+                    R.string.cart
+                )
+            )
             startActivity(intent)
         }
         button3.setOnClickListener {
             IdBtn = 3
             changeTextColors(IdBtn)
-            val intent = Intent(this, SecondActivity::class.java)
-            intent.putExtra(EXTRA_Data, SomeData(IdBtn, R.drawable.r, R.string.R_text))
+            val intent = Intent(this, FilmDetals::class.java)
+            intent.putExtra(
+                EXTRA_Data, FilmData(
+                    IdBtn,
+                    R.drawable.r,
+                    R.string.R_text,
+                    R.string.rock
+                )
+            )
             startActivity(intent)
         }
 
@@ -80,8 +115,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-         outState.putInt(IDFILM, IdBtn)
+        outState.putInt(IDFILM, IdBtn)
     }
+
 
 }
 
